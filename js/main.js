fd96568f5bfa74782a6feef3b4d35962394214ef
fatima <fatima.jacoboruiz@gmail.com>
2016-10-06 17:36:49 -0500

    version 0.0.2

--- js/main.js
+++ js/main.js
@@ -18,4 +18,19 @@ function addLista(){
 	span.classList.add("none");
 	var entrada = document.getElementById("entrada");
 }
+function addTitulo(){
+ 	form.style.display="none"; 
+	var titulo = document.createElement("div");
+	titulo.innerText = entrada.value;
+	tarjeta.appendChild(titulo);
+	span.parentElement.insertBefore(titulo, span.parentElement.childNodes[0]);
+	entrada.value = "";
+
+	var link = document.createElement("a");
+	link.setAttribute("href","#")
+	var txt = document.createTextNode("Añadir una tarjeta..."); 
+	link.appendChild(txt);
+	span.parentElement.insertBefore(link, span.parentElement.childNodes[1]);
+	link.addEventListener("click", addTextArea);
+}
 
